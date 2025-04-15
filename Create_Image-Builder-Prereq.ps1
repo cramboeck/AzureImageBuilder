3292231a483456b0de0b94c97f3106309d77bff2
cramboeck <131172187+cramboeck@users.noreply.github.com>
2025-04-15 23:14:35 +0200

    Update Create_Image-Builder-Prereq.ps1
    Add Download from Azure Storage Account

--- Create_Image-Builder-Prereq.ps1
+++ Create_Image-Builder-Prereq.ps1
@@ -3,3 +3,8 @@ New-Item -Type Directory -Path 'c:\\' -Name ImageBuilder,
 invoke-webrequest -uri 'https://aka.ms/downloadazcopy-v10-windows' -OutFile 'c:\\ImageBuilder\\azcopy.zip',
 Expand-Archive 'c:\\ImageBuilder\\azcopy.zip' 'c:\\ImageBuilder',
 copy-item 'C:\\ImageBuilder\\azcopy_windows_amd64_*\\azcopy.exe\\' -Destination 'c:\\ImageBuilder'
+
+# Inline command that uses AZCopy to download the archive file and extract to the ImageBuilder directory
+# Use the SAS URL for the <ArchiveSource>
+c:\\ImageBuilder\\azcopy.exe copy '<ArchiveSource>' c:\\ImageBuilder\\software.zip,
+Expand-Archive 'c:\\ImageBuilder\\software.zip' c:\\ImageBuilder

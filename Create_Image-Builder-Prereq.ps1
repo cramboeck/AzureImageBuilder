4f79774b47473266848e2945f2491d2ca12afb4d
cramboeck <131172187+cramboeck@users.noreply.github.com>
2025-04-21 11:57:40 +0200

    Update Create_Image-Builder-Prereq.ps1

--- Create_Image-Builder-Prereq.ps1
+++ Create_Image-Builder-Prereq.ps1
@@ -6,5 +6,5 @@ copy-item 'C:\\ImageBuilder\\azcopy_windows_amd64_*\\azcopy.exe\\' -Destination 
 
 # Inline command that uses AZCopy to download the archive file and extract to the ImageBuilder directory
 # Use the SAS URL for the <ArchiveSource>
-c:\\ImageBuilder\\azcopy.exe copy '<ArchiveSource>' c:\\ImageBuilder\\software.zip,
+c:\\ImageBuilder\\azcopy.exe copy 'https://ramboeckit.blob.core.windows.net/azureimagebuilder/Software.zip?sp=r&st=2025-04-21T09:56:50Z&se=2025-04-21T17:56:50Z&spr=https&sv=2024-11-04&sr=b&sig=DuQ1FfBRdNpLwBqvFjRWIRqxGCysoUuWZiDByVhaMNk%3D' c:\\ImageBuilder\\software.zip,
 Expand-Archive 'c:\\ImageBuilder\\software.zip' c:\\ImageBuilder
